3d623e06f4a97126f7466f74916e744a1d32019d
julianorocket <julianoprogrammierer@gmail.com>
2020-08-16 00:47:07 +0000

    starting landing page features

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,29 +1,23 @@
 import React from 'react';
-import logoimg from '../../assets/images/logo.svg';
+
 
 
 function Landing(){
 
-    return (
-        <div id="page-landing">
-            <div id="page-landing-content" className="container">
-                <img src="" alt=""/>
-
-
-
-            </div>
-
-
-
-
-
-        </div>
-         
+    return (<h1>Landing</h1>
     
+
+
 
 
 
     )
 }
+
+
+
+
+
+
 
 export default Landing;
